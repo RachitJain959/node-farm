390c2f39528371d313155a90b103b3e220df8610
Rachit Jain <rachitjain9596@gmail.com>
2025-01-10 21:37:24 +0530

    html template filling

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { log } = require("console");
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
@@ -31,6 +32,22 @@ const url = require("url");
 
 // SERVER
 
+const replaceTemplate = (temp, product) => {
+	let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+	output = output.replace(/{%IMAGE%}/g, product.image);
+	output = output.replace(/{%PRICE%}/g, product.price);
+	output = output.replace(/{%FROM%}/g, product.from);
+	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+	output = output.replace(/{%QUANTITY%}/g, product.quantity);
+	output = output.replace(/{%DESCRIPTION%}/g, product.description);
+	output = output.replace(/{%ID%}/g, product.id);
+
+	if (!product.organic) {
+		output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+	}
+	return output;
+};
+
 const tempOverview = fs.readFileSync(
 	`${__dirname}/templates/template-overview.html`,
 	"utf-8"
@@ -54,9 +71,12 @@ const server = http.createServer((req, res) => {
 	if (pathName === "/" || pathName === "/overview") {
 		res.writeHead(200, { "content-type": "text/html" });
 
-		// const cardsHtml = dataObj.map(el)
+		const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el));
+		// console.log(cardsHtml);
 
-		res.end(tempOverview);
+		const output = tempOverview.replace("{%PRODUCT_CARD%}", cardsHtml);
+
+		res.end(output);
 
 		// Product
 	} else if (pathName === "/product") {
