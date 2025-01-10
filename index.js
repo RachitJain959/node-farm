917b36e5c636b10dabce1f1c91aae43b40c5c517
Rachit Jain <rachitjain9596@gmail.com>
2025-01-10 21:00:59 +0530

    html template filling

--- index.js
+++ index.js
@@ -31,16 +31,38 @@ const url = require("url");
 
 // SERVER
 
+const tempOverview = fs.readFileSync(
+	`${__dirname}/templates/template-overview.html`,
+	"utf-8"
+);
+const tempProduct = fs.readFileSync(
+	`${__dirname}/templates/template-product.html`,
+	"utf-8"
+);
+const tempCard = fs.readFileSync(
+	`${__dirname}/templates/template-card.html`,
+	"utf-8"
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
 	const pathName = req.url;
 
+	// Overview
 	if (pathName === "/" || pathName === "/overview") {
-		res.end("overview");
+		res.writeHead(200, { "content-type": "text/html" });
+
+		// const cardsHtml = dataObj.map(el)
+
+		res.end(tempOverview);
+
+		// Product
 	} else if (pathName === "/product") {
 		res.end("product");
+
+		// API
 	} else if (pathName === "/api") {
 		res.writeHead(200, {
 			"content-type": "application/json",
